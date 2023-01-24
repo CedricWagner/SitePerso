5eb8db4e0b58eee61911baeb7ff27e4725512bb4
Cédric Wagner <cwagner@tiz.fr>
2023-01-24 14:38:19 +0100

    feature: Experience: improve design of items

--- src/features/Experiences/components/Item/Item.tsx
+++ src/features/Experiences/components/Item/Item.tsx
@@ -9,11 +9,11 @@ export const Item: FC<ExperienceType> = ({
   role,
   description,
 }) => (
-  <li data-testid="ExperienceItem">
-    <h4 className="text-lg font-bold">{organization}</h4>
-    <span>{type}</span>
-    <span>{duration}</span>
-    <span>{role}</span>
+  <li data-testid="ExperienceItem" className="mb-4">
+    <h4 className="inline text-lg font-bold">{organization}</h4>,
+    <span className="pl-1 after:pl-1 after:content-['—']">{type}</span>
+    <span className="pl-1 italic">{role}</span>
+    <p className="size-sm mb-1 text-slate-400">{duration}</p>
     <DangerousHtmlContainer html={description} className="text-editor" />
   </li>
 );
